c352840d6daccbd7108e0e10a3fe84cb5ef5dc06
2709patri <128839994+2709patri@users.noreply.github.com>
2023-04-16 20:41:06 -0300

    borrado index.js

---
+++ src/js/cargar-componentes.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+  console.log("Script cargar-componentes.js cargado");
+  $("header").load("../components/header.html");
+  $("footer").load("../components/footer.html");
+});
